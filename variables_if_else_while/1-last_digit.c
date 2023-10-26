5df19663e8005636a06b23269b23382a7d310a37
Reah2 <jamesramoshabe5@gmail.com>
2023-10-26 10:50:44 -0700

    1-last_digit commit

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -18,11 +18,11 @@ int n;
 srand(time(0));
 n = rand();
 
-printf("Last digit of %d is ", n);
+printf("Last digit of %d is ", n % 10);
 
 if (n % 10 > 5)
 {
-printf("%d and is greater than 5\n", n % 10);
+printf("%d and is greater than 5\n", n);
 }
 else if (n % 10 == 0)
 {
@@ -30,7 +30,7 @@ printf("0 and is 0\n");
 }
 else
 {
-printf("%d and is less than 6 and not 0\n", n % 10);
+printf("%d and is less than 6 and not 0\n", n);
 }
 return (0);
 }
