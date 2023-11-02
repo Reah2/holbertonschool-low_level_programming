4b65e8a1215d05ae4d7cbe488ff40c37ffce6914
Reah2 <jamesramoshabe5@gmail.com>
2023-11-02 09:30:09 -0700

    create_static_lib.sh

---
+++ static_libraries/_strspn.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * _strspn - Function that gets the length of the prefix substring
+ * @s: string to search in
+ * @accept: bytes to search for
+ * Return: number of bytes in initial segments
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, z, count, checker;
+
+	i = 0;
+	z = 0;
+	checker = 0;
+	count = 0;
+
+	while (s[i] != '\0')
+	{
+		z = 0;
+		checker = 0;
+
+		while (accept[z] != '\0')
+		{
+			if (accept[z] == s[i])
+			{
+				count++;
+				checker = 1;
+				break;
+			}
+			z++;
+		}
+
+		if (checker == 0)
+			return (count);
+
+				i++;
+	}
+	return (count);
+}
