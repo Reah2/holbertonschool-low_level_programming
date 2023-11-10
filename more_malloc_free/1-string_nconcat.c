63f186fd94d74aca99f72ca565bfd8a244f39762
Reabetsoe James Ramoshabe <147239450+Reah2@users.noreply.github.com>
2023-11-10 14:13:46 +0200

    Update 1-string_nconcat.c

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -1,24 +1,36 @@
+#include <stdlib.h>
 #include "main.h"
 
 /**
-* string_nconca - Concatenates two strings using at most
-* an inputted number of bytes from s1.
-*
-* @s1: The string to be appended upon.
-*
-* @s2: The string to be appended to s1.
-*
-* @n: The number of bytes from s1 to be appended to s2.
-*
-*Return: A pointer to the resulting string s1.
-*/
+ * string_nconcat - Function that concats two strings but with s2 by n amount.
+ * @s1: base string to concat to. 'NULL' will return as empty
+ * @s2: string to copy from 'NULL' will return as empty string
+ * @n: Amount of s2 to copy
+ * Return: Pointer to allocate memory
+ */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-unsigned int index = 0, s1_len = 0;
-
-while (s1[index++])
-s1_len++;
-for (index = 0; s2[index] && index < n; index++)
-s1[s1_len++] = s2[index];
-return (s1);
+char *str;
+unsigned int i, a, b;
+a = b = 0;
+if (s1)
+for (; s1[a]; a++)
+;
+if (s2)
+for (; s2[b]; b++)
+;
+if (!(n >= b))
+b = n;
+str = malloc(sizeof(char) * (a + b)+1);
+if (!str)
+return (NULL);
+for (i = 0; i < (a + b); i++)
+{
+if (i < a)
+str[i] = s1[i];
+else
+str[i] = s2[i - a];
+}
+str[i] = '\0';
+return (str);
 }
