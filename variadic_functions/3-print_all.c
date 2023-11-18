164d5c820759d5d73c127f9638a755326cc887c7
Reah2 <jamesramoshabe5@gmail.com>
2023-11-18 10:25:13 -0800

    3-print_all.c

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -32,13 +32,14 @@ void print_all(const char * const format, ...)
 				case 's':
 				      str = va_arg(list, char *);
 				      if (!str)
-					      str = "(nil)";
+						str = "(nil)";
 					printf("%s%s", sep, str);
-				      break;
+				      	break;
 				default:
-				      	i++;					continue;
+				      	i++;
+					continue;
 			}
-			sep = ",";
+			sep = " ,";
 			i++;
 		}
 	}
