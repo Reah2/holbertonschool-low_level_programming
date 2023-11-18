0aa444062a61919afc7f1111f6d7ea6336702bdf
Reabetsoe James Ramoshabe <147239450+Reah2@users.noreply.github.com>
2023-11-18 22:39:26 +0200

    Update 3-op_functions.c

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -47,11 +47,11 @@ return (a * b);
 */
 int op_mod(int a, int b)
 {
-  if (b == 0)
-    {
-        printf("Error\n");
-        exit(100);
-    }
+if (b == 0)
+{
+printf("Error\n");
+exit(100);
+}
 return (a % b);
 }
 
@@ -64,10 +64,10 @@ return (a % b);
 */
 int op_div(int a, int b)
 {
-  if (b == 0)
-    {
-        printf("Error\n");
-        exit(100);
-    }
+if (b == 0)
+{
+printf("Error\n");
+exit(100);
+}
 return (a / b);
 }
