11ea1ad5418ad0f94a1ec4fe26056dd7db7dbfb7
Reabetsoe James Ramoshabe <147239450+Reah2@users.noreply.github.com>
2023-11-18 22:35:13 +0200

    Update 3-op_functions.c

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -3,48 +3,48 @@
 #include <stdlib.h>
 
 /**
- * op_add - runs addition calculation of a and b
- * @a: int value
- * @b: int value
- *
- * Return: Result
- */
+* op_add - runs addition calculation of a and b
+* @a: int value
+* @b: int value
+*
+* Return: Result
+*/
 int op_add(int a, int b)
 {
 return (a + b);
 }
 
 /**
- * op_sub - runs substraction calculation of a and b
- * @a: int value
- * @b: int value
- *
- * Return: result
- */
+* op_sub - runs substraction calculation of a and b
+* @a: int value
+* @b: int value
+*
+* Return: result
+*/
 int op_sub(int a, int b)
 {
 return (a - b);
 }
 
 /**
- * op_mul - runs multiplication calculation of a and b
- * @a: int value
- * @b: int value
- *
- * Return: result
- */
+* op_mul - runs multiplication calculation of a and b
+* @a: int value
+* @b: int value
+*
+* Return: result
+*/
 int op_mul(int a, int b)
 {
 return (a * b);
 }
 
 /**
- * op_mod - runs module calculation of a and b
- * @a: int value
- * @b: int value
- *
- * Return: result
- */
+* op_mod - runs module calculation of a and b
+* @a: int value
+* @b: int value
+*
+* Return: result
+*/
 int op_mod(int a, int b)
 {
   if (b == 0)
@@ -56,12 +56,12 @@ return (a % b);
 }
 
 /**
- * op_div - runs division calculation of a and b
- * @a: int value
- * @b: int value
- *
- * Return: result
- */
+* op_div - runs division calculation of a and b
+* @a: int value
+* @b: int value
+*
+* Return: result
+*/
 int op_div(int a, int b)
 {
   if (b == 0)
