8c9f437bd412d4086382a6289fab70f849ceb0e0
Reabetsoe James Ramoshabe <147239450+Reah2@users.noreply.github.com>
2023-11-18 22:44:54 +0200

    Update 4-new_dog.c

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,51 +1,53 @@
-#include <stdlib.h>
 #include "dog.h"
+#include <stdlib.h>
 
 /**
- * new_dog - creates a new dog
- * @name: name of the dog
- * @age: age of teh dog
- * @owner: owner of the dog
- *
- * Return: pointer to the new dog (success), NULL otherwise
+ * _strcopy - copy read only data
+ * @dst: pointer to copy char
+ * @src: read data
  */
+void _strcopy(char *dst, char *src)
+{
+int i;
+for (i = 0; src[i]; i++)
+dst[i] = src[i];
+dst[i] = '\0';
+}
 
+/**
+ * new_dog - create new dog from the dna of the first dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Return: pointer to dog
+ */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *dog;
-	unsigned int len1, len2, i;
-
-	if (name == NULL || owner == NULL)
-		return (NULL);
-
-	dog = malloc(sizeof(dog_t));
-	if (dog == NULL)
-		return (NULL);
-	for (len1 = 0; name[len1]; len1++)
-		len1++;
-
-	dog->name = malloc(len1 * sizeof(char));
-	if (dog->name == NULL)
-	{
-		free(dog);
-		return (NULL);
-	}
-
-	for (i = 0; i < len1; i++)
-		dog->name[i] = name[i];
-	dog->age = age;
-
-	for (len2 = 0; owner[len2]; len2++)
-		len2++;
-
-	dog->owner = malloc(len2 * sizeof(char));
-	if (dog->owner == NULL)
-	{
-		free(dog->name);
-		free(dog);
-		return (NULL);
-	}
-	for (i = 0; i < len2; i++)
-		dog->owner[i] = owner[i];
-	return (dog);
+dog_t *d;
+int a, b;
+for (a = 0; name[a]; a++)
+;
+for (b = 0; owner[b]; b++)
+;
+d = malloc(sizeof(dog_t));
+if (!d)
+return (NULL);
+d->name = malloc(a + 1);
+if (!d->name)
+{
+free(d);
+return (NULL);
+}
+d->owner = malloc(b + 1);
+if (!d->owner)
+{
+free(d->name);
+free(d);
+return (NULL);
+}
+_strcopy(d->name, name);
+_strcopy(d->owner, owner);
+d->age = age;
+return (d);
 }
