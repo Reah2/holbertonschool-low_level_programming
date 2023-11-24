9596be99bbb88fd66ae628f955d1934fdc4d0b6a
Reabetsoe James Ramoshabe <147239450+Reah2@users.noreply.github.com>
2023-11-24 19:08:53 +0200

    Update 0-strcat.c

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,44 +1,24 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
+#include "main.h"
 
 /**
- * main - Prints if number is positive, zero or negative
- *
- * Return: Always (Success)
- */
-
-int main(void)
-{int n;
-
-	srand(time(0));
-
-	n = rand() - RAND_MAX / 2;
-
-	if (n > 0)
-
-	{
-
-		printf("%d is positive\n", n);
-
-	}
-
-	else if (n == 0)
-
-	{
-
-		printf("%d is zero\n", n);
-
-	}
-
-	else
-
-	{
-
-		printf("%d is negative\n", n);
-
-	}
-
-	return (0);
-
+* _strcat - This function will concatenate two strings
+* @dest: The string that will modify
+* @src:Thestring that will be concatenate to @des
+* Return: a pointer to @dest
+*/
+char *_strcat(char *dest, char *src)
+{
+int index_01 = 0;
+int index_02 = 0;
+while (dest[index_01] != '\0')
+{
+index_01++;
+}
+while (src[index_02] != '\0')
+{
+dest[index_01 + index_02] = src[index_02];
+index_02++;
+}
+dest[index_01 + index_02] = '\0';
+return (dest);
 }
