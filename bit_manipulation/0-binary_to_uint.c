451d67abd5d37ee6ed2680591d8202196c03d079
Reah2 <jamesramoshabe5@gmail.com>
2023-12-03 05:12:34 -0800

    commit

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -34,7 +34,7 @@ for (i = 0; b[i] != '\0'; i++)
 if (b[i] != '0' && b[i] != '1')
 return (0);
 for (j = len - 1; j > 0; j--)
-pow = pow * base;
+pow = pow *base;
 sum = sum + (pow * (b[i] - 48));
 len--;
 pow = 1;
