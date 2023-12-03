7c17c803f1c8cb4aadb8c3f34571771f5f50a0fa
Reabetsoe James Ramoshabe <147239450+Reah2@users.noreply.github.com>
2023-12-03 14:08:26 +0200

    Create 3-set_bit.c

---
+++ bit_manipulation/3-set_bit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* set_bit - set the bit of a number to 1
+*
+* @n: pointer to the bit to be manipulated
+* @index: position of the bit to be manippulated
+* Return: 1 on success or -1 on failed
+*/
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+unsigned int mask;
+
+if (index > sizeof(unsigned int) * 8)
+return (-1);
+
+mask = 1;
+mask = mask << index;
+*n = ((*n) | mask);
+return (1);
+}
