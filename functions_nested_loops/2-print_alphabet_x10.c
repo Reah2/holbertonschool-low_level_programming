0190b0de37b9ad3ba3146b1305ee7df383068b06
Reabetsoe James Ramoshabe <147239450+Reah2@users.noreply.github.com>
2023-12-05 19:50:43 +0200

    Update 2-print_alphabet_x10.c

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,14 +1,23 @@
 #include "main.h"
 
 /**
-* print_alphabet - Function to print the alphabet.
+* print_alphabet_x10 - Make alphabet x10 times
+* Return: void
 */
 
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
 char c;
-
+int num;
+num = 0;
+while (num < 10)
+{
 for (c = 'a'; c <= 'z'; c++)
+{
 _putchar(c);
+}
 _putchar('\n');
+num++;
+}
+
 }
