f2dc6480381d07608f9c829da7d09e0519a109b1
Reabetsoe James Ramoshabe <147239450+Reah2@users.noreply.github.com>
2023-12-05 18:26:34 +0200

    Update 5-printf.c

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,1 +1,10 @@
-
+#include <stdio.h>
+/**
+* main - A program that prints a line using printf function
+* Return: 0 (success)
+*/
+int main(void)
+{
+printf("Progamming is like building a multilingual puzzle,\n");
+return (0);
+}
